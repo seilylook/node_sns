b7c0d2c4882d24ef9652b5a5cc1983b0735707e2
seilylook <seilylook@naver.com>
2023-01-14 14:46:22 +0900

    setting packages and start port

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cookieparser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const pageRouter = require('./routes/page');
+
+const app = express();
+
+app.set('port', process.env.PORT || 8001);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieparser(process.env.COOKIE_PARSER));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_PARSER,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use(flash());
+
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'developement' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
