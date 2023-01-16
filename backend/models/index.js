8947f156a5139b0919214975653fcde1ec2d5bba
seilylook <seilylook@naver.com>
2023-01-16 12:10:58 +0900

    passport를 이용한 로그인 기능 구현 && 카카오는 수정 필요

--- backend/models/index.js
+++ backend/models/index.js
@@ -12,23 +12,19 @@ const sequelize = new Sequelize(
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
 db.Hashtag = require('./hashtag')(sequelize, Sequelize);
 
 db.User.hasMany(db.Post);
 db.Post.belongsTo(db.User);
-
 db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
 db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
-
 db.User.belongsToMany(db.User, {
   foreignKey: 'followingId',
   as: 'Followers',
   through: 'Follow',
 });
-
 db.User.belongsToMany(db.User, {
   foreignKey: 'followerId',
   as: 'Followings',
